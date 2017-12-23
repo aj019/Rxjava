e3ad29e0c745b9fa0129dfdb579a0c01e3d04065
Anuj Gupta <junagupta19@gmail.com>
2017-12-23 12:56:50 +0530

    Created Observables like Observable,Flowable,Maybe,Single

--- app/src/main/java/in/androidmate/rxjava/MainActivity.java
+++ app/src/main/java/in/androidmate/rxjava/MainActivity.java
@@ -1,68 +1,143 @@
 package in.androidmate.rxjava;
 
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 
-import in.androidmate.rxjava.Network.NetworkCallback;
-import in.androidmate.rxjava.Network.NetworkStore;
-import in.androidmate.rxjava.Network.NetworkClient;
-import rx.Observable;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-import rx.subscriptions.CompositeSubscription;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import io.reactivex.Maybe;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MainActivity";
-    private Subscription subscription;
-    private TextView tv;
-    CompositeSubscription compositeSubscription;
+    Subscription s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = (TextView) findViewById(R.id.tv);
+        Observable<Integer> getObservable =  Observable.just(1,2);
+        Flowable<Integer> flowObservable = Flowable.just(3,4);
+        Single<Integer> singleObservable = Single.just(5);
+        Maybe<Integer> maybeObservable = Maybe.just(6);
+//        Completable completable = Completable.amb();
 
-        Observable<WeatherData> fetchCurrentWeather = NetworkClient.getRetrofit(this)
-                .create(NetworkStore.class)
-                .fetchCurrentWeather("London,uk","b1b15e88fa797225412429c1c50c122a1")
+
+         Observer<Integer> getObserver  = new Observer<Integer>() {
+
+             @Override
+             public void onSubscribe(Disposable d) {
+
+             }
+
+             @Override
+                public void onNext(Integer value) {
+                    Log.d(TAG,String.valueOf(value));
+                }
+
+                @Override
+                public void onError(Throwable e) {
+
+                }
+
+                @Override
+                public void onComplete() {
+                    Log.d(TAG,"onCompleted");
+                }
+            };
+
+           Subscriber<Integer> flowableObserver = new Subscriber<Integer>() {
+
+               @Override
+               public void onSubscribe(Subscription s) {
+                   Log.d(TAG,"Flow Subscriber subscribed");
+               }
+
+               @Override
+               public void onNext(Integer integer) {
+                   Log.d(TAG,String.valueOf(integer));
+               }
+
+               @Override
+               public void onError(Throwable t) {
+                   Log.d(TAG,t.toString());
+               }
+
+               @Override
+               public void onComplete() {
+                   Log.d(TAG,"Flow Subscriber Completed");
+
+               }
+           };
+
+           Consumer<Integer> singleObserver = new Consumer<Integer>() {
+
+               @Override
+               public void accept(Integer integer) throws Exception {
+                   if(integer != null){
+                       Log.d(TAG,"Single Observer "+integer);
+                   }
+               }
+           };
+
+            Consumer<Integer> maybeObserver  = new Consumer<Integer>() {
+
+
+                @Override
+                public void accept(Integer integer) throws Exception {
+                    Log.d(TAG,"Maybe Observer "+integer);
+                }
+            };
+
+
+        getObservable
                 .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(getObserver);
+
+        flowObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(flowableObserver);
+
+        singleObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(singleObserver);
+
+        maybeObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(maybeObserver);
 
 
-        subscription = fetchCurrentWeather.subscribe(new NetworkCallback<WeatherData>() {
-            @Override
-            public void onSuccess(WeatherData model) {
-                Log.d(TAG,"Success");
-                tv.setText(model.getName());
-            }
 
-            @Override
-            public void onFailure(String message) {
-                Log.d(TAG,"Failure");
-            }
 
-            @Override
-            public void onFinish() {
-                Log.d(TAG,"Finish");
-            }
-        });
+
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        if(subscription != null && !subscription.isUnsubscribed()){
-            subscription.unsubscribe();
-        }
+        s.cancel();
 
     }
 
